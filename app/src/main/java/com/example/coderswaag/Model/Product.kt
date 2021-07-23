5c3286887df9e8cd87eab2677c70fbd0a911c88b
Kunal <kunalv1234567890@gmail.com>
2021-07-23 11:11:33 +0530

    CoderSwag App

---
+++ app/src/main/java/com/example/coderswaag/Model/Product.kt
@@ -0,0 +1,3 @@
+package com.example.coderswaag.Model
+
+class Product(val title:String, val prize:String, val image:String )
---
+++ app/src/main/java/com/example/coderswaag/Services/DataServices.kt
@@ -0,0 +1,36 @@
+package com.example.coderswaag.Services
+
+import com.example.coderswaag.Model.Category
+import com.example.coderswaag.Model.Product
+
+object DataServices {
+
+    val Categories = listOf(
+        Category("shirts", "shirtimage"),
+        Category ("hats" , "hatimage"),
+        Category("Hoodies" , "hoddieimage"),
+        Category("digital" , "digitaligoodsmage")
+
+    )
+
+    val Hats = listOf(
+        Product(" dope", "22$" , "hats01"),
+        Product(" weed boy", "21$" , "hats02"),
+        Product(" RDX", "20$" , "hats03"),
+        Product(" Gun Snoorr", "24$" , "hats04")
+    )
+
+    val hoodies = listOf(
+                Product("void Class" ,"29$" , "hoodie01"),
+                Product(" Hilfighter" ,"22$" , "hoodie02"),
+                Product("Puma" ,"25$" , "hoodie03"),
+                Product("USP Asian" ,"24$" , "hoodie04")
+    )
+
+    val shirts = listOf(
+        Product("white denim" , "10$" , "shirt01"),
+        Product("white denim" , "10$" , "shirt01"),
+
+
+        )
+}
